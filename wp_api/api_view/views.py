a386875f7289c93119e68e3bb7e55f4e1dd961ff
Wojciech Pragłowski <wojtekpraglo@gmail.com>
2021-12-26 17:28:36 +0100

    Stworzenie forms ze zbieraniem username i user email

--- wp_api/api_view/views.py
+++ wp_api/api_view/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import ViewForm
 
 # Create your views here.
+
+def index(request):
+    if request.method == "POST":
+        form = ViewForm(request.POST)
+
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect("/thank-you")
+
+    form = ViewForm()
+    return render(request, "api_view/index.html", {
+        "form": form
+    })
+
+
+def thank_you(request):
+    return render(request, "api_view/thank-you.html", {})
